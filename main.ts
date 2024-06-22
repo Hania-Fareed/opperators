c3c7b5d7d69a4f23f0ca19a603ea826ef7e58c21
Hania-Fareed <haniafareed7@gmail.com>
2024-06-22 18:10:40 +0500

    opperators

---
+++ main.ts
@@ -0,0 +1,29 @@
+let num1: number = 2;
+let num2: number = 3;
+let cart: number = num1 + num2;
+console.log(cart)
+
+let num3: number = 5;
+let eaten: number = 2;
+let total: number = num3 - eaten;
+console.log(total);
+
+let appleBag: number = 4;
+let apple: number = 5;
+let totalApple: number = appleBag * apple;
+console.log(totalApple)
+
+let distributedApple: number = 20;
+let bags: number = 4;
+let eachBag: number = distributedApple / bags;
+console.log(eachBag)
+
+let layers: number = 5;
+let appleslayers: number = 5;
+let power: number = layers ** 2;
+console.log(power)
+
+let appledistributed: number = 5;
+let AppleBags: number = 2;
+let remainder: number = appledistributed % AppleBags;
+console.log(remainder)
